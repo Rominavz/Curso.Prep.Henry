4f783bce83c0f43ee5200499a9e68cd0b76a9f28
Romina <rominaevalde@gmail.com>
2021-11-19 08:37:30 -0300

    guardado nuevamente

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -119,10 +119,10 @@ function pasarUsuarioAPremium(usuarios) {
   // Define cada propiedad "esPremium" de cada objeto como "true"
   // Devuelve el array de usuarios
   // Tu código:
-  for (var i=0; i<usuarios.length; i++) {
+    for (var i = 0; i < usuarios.length; i++) {
     usuarios[i].esPremiun = true;
   }
-   return usuarios;
+    return usuarios;
 }
 
 function sumarLikesDeUsuario(usuario) {
